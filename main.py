7d17a34b5d09e91f20288cce5f23c2087111e267
R. Kyle Norris <r.kyle.norris@gmail.com>
2026-04-16 20:04:12 -0400

    setup main to test daylio tracker ETL

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from pathlib import Path
+from trackers.mind import DaylioTracker, OldDataException
+
+
 def main():
-    print("Hello from self-studio-warehouse!")
+    print("Starting ETL...")
+    backup_dir = Path(r"C:\Users\roder\OneDrive\ToolData\DaylioData")
+    tracker = DaylioTracker(backup_path=backup_dir)
+    try:
+        tracker.get_data().ingest_data()
+    except OldDataException as e:
+        print(f"{e}")
+    except FileNotFoundError as e:
+        print(f"{e}")
+    finally:
+        print("Finished.")
 
 
 if __name__ == "__main__":
